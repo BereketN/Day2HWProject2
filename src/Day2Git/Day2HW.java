6dac5c03efd935017c58b3ebf15b18e12090c2b2
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-10 13:53:19 -0700

    Day 2 Commit

---
+++ src/Day2Git/Day2HW.java
@@ -0,0 +1,11 @@
+package Day2Git;
+
+public class Day2HW {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Local repository");
+		
+		System.out.println("Remote repository");
+	}
+}
